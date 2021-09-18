35fb85e7d2f994cf7a74a4e3d5516430ac183ecf
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2021-09-18 19:52:42 +0530

    Mihiranga Changed

---
+++ app/src/main/java/com/builditmasters/maddkuppiya/Mihiranga.java
@@ -0,0 +1,14 @@
+package com.builditmasters.maddkuppiya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mihiranga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mihiranga);
+    }
+}
