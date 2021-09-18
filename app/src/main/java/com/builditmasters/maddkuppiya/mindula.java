da73de5f9932c48afc199b6e75e6bf7c36441f7d
Mindula98 <mywanniarachchi@gmail.com>
2021-09-18 19:55:33 +0530

    mindula java file created

---
+++ app/src/main/java/com/builditmasters/maddkuppiya/mindula.java
@@ -0,0 +1,14 @@
+package com.builditmasters.maddkuppiya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mindula extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mindula);
+    }
+}
